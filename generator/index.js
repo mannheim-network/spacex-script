5e5c81b46d3c538a010cd01e5d3bbf5bebc5dcf6
blocker-post <mannheimnetwork@gmail.com>
2021-12-30 20:34:24 +0800

    add generator config

---
+++ generator/index.js
@@ -0,0 +1,67 @@
+
+const _ = require('lodash')
+const fs = require('fs-extra')
+const path = require('path')
+const yaml = require('js-yaml')
+const { genConfig, genComposeConfig } = require('./config-gen')
+const { getConfigSchema } = require('./schema')
+const { validate } = require('./config-validator')
+const { logger } = require('./logger')
+const { inspectKey } = require('./key-utils')
+const { writeYaml } = require('./utils')
+
+console.log('Spacex config generator')
+
+async function loadConfig(file) {
+  logger.debug('Loading config file: %s', file)
+  const c = await fs.readFile('config.yaml', 'utf8')
+  const config = yaml.safeLoad(c)
+  const configSchema = getConfigSchema(config)
+  const value = await configSchema.validateAsync(config, {
+    allowUnknown: true,
+    stripUnknown: true,
+  })
+
+  if (value.node.storage == "enable") {
+    const keyInfo = await inspectKey(value.identity.backup.address)
+    value.identity.account_id = keyInfo.accountId
+  }
+
+  const data = await genConfig(value, {
+    baseDir: '.tmp',
+  })
+  const composeConfig = await genComposeConfig(value)
+  await writeYaml(path.join('.tmp','docker-compose.yaml'), composeConfig)
+  await dumpConfigPaths(path.join('.tmp', '.paths'), data)
+}
+
+async function dumpConfigPaths(toFile, data) {
+  const paths = _(data).map(d => _.get(d, 'paths', [])).flatten().map(p => {
+    let mark = '|'
+    if (p.required) {
+      mark = '+'
+    }
+    return `${mark} ${p.path}`
+  }).uniq()
+
+  await fs.outputFile(toFile, paths.join('\n'))
+}
+
+function getConfigFileName() {
+  const args = process.argv.slice(2);
+  if (args.length >= 1) {
+    return args[0]
+  }
+  return 'config.yaml'
+}
+
+async function main(){
+  try {
+    await loadConfig(getConfigFileName())
+  } catch(e) {
+    logger.error('failed to load config: %o', e.message)
+    process.exit(1)
+  }
+}
+
+main()
---
+++ generator/key-utils.js
@@ -0,0 +1,37 @@
+const _ = require('lodash')
+const execa = require('execa')
+const { logger } = require('./logger')
+
+async function inspectKey(address) {
+  const keyTool = process.env['KEY_TOOL']
+  if (!keyTool) {
+    throw 'key tool path not specified'
+  }
+  const {stdout} = await execa(keyTool, ['inspect', address]);
+
+  const rows = _.chain(stdout).split('\n').map(_.trim)
+  const accountId = extractAccountId(rows)
+  if (!accountId) {
+    logger.warn('Invalid address: %s!', address)
+    throw `address is invalid ${address}`
+  }
+  return {
+    address,
+    accountId,
+  }
+}
+
+function extractAccountId(outputs) {
+  const accountIdPrefix = 'Account ID:'
+  const ids = outputs.filter(v =>_.startsWith(v, accountIdPrefix)).map(v => v.substr(accountIdPrefix.length)).map(_.trim).value()
+  if (_.isEmpty(ids)) {
+    return null
+  }
+
+  return ids[0].substring(2)
+}
+
+
+module.exports = {
+  inspectKey,
+}
